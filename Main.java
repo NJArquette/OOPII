463dc2be4880aa7a3af103344f5998af311d3003
NJArquette <39813294+NJArquette@users.noreply.github.com>
2018-09-25 18:32:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,37 @@
+/*Nathaniel Arquette
+*class project
+*The goal of this piece of code is to 
+*show an understanding of using file extensions
+*and overrides
+*/
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+//what do we want?
         System.out.println("Please enter a basic color to test.");
         Scanner sc = new Scanner(System.in);
         String test = sc.next();
 
+        //tell em what they entered
         Test(test);
 
+        //we want more
         System.out.println("Please enter another basic color to test.");
         String test2 =sc.next();
 
+        //tell em what they entered
         Test(test2);
 
+        //magic is gonna happen!
         System.out.println("Mixing colors.");
 
         Test call = new Test();
         call.Test(test, test2);
     }
 
+    //this is how we tell them what they entered
     private static void Test(String test) {
 
         if (test.equalsIgnoreCase("blue")||
